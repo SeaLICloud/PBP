9d94157a0c6a11dd50240f84f95db1c802debc2e
lihai <171220531@qq.com>
2019-04-19 15:27:12 +0800

    修复分页查询bug

--- PBP.Web/Controllers/AccountController.cs
+++ PBP.Web/Controllers/AccountController.cs
@@ -31,7 +31,9 @@ namespace PBP.Web.Controllers
             ViewData["CurrentSort"] = sortOrder;
             ViewData["NameSortParm"] = string.IsNullOrEmpty(sortOrder) ? "name_desc" : string.Empty;
             ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+
             ViewData["Total"] = context.Accounts.Count();
+
             ViewData["CurrentPage"] = pageNumber;
 
             if (searchString != null)
@@ -48,6 +50,7 @@ namespace PBP.Web.Controllers
             if (!string.IsNullOrEmpty(searchString))
             {
                 accounts = accounts.Where(s => s.UserName.Contains(searchString));
+                ViewData["Total"] = accounts.Count();
             }
 
             switch (sortOrder)
